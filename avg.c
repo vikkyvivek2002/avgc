62171e310dea0cef1e597a9de14924fe6398ae97
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-07-13 00:00:18 +0530

    Add files via upload

---
+++ avg.c
@@ -0,0 +1,12 @@
+#include<Stdio.h>
+int main()
+{
+	int a,b,c,avg;
+	printf("Enter three number :");
+	scanf("%d%d%d",&a,&b,&c );
+	avg = (a+b+c)/3;
+	printf("\nAverage of the three numbers is = %d\n", avg);
+	return 0;
+	
+	
+}
